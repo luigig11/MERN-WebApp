a41ac3935e91deeb7b6ab02f1d9c2ee123ff99b9
LuisGarcia <luis_garcia_11@outlook.es>
2021-02-13 13:06:52 -0600

    Se incluyen métodos de autenticacion al modelo de usuario

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -26,4 +26,35 @@ const UserSchema = new mongoose.Schema({
 
 })
 
+UserSchema
+    .virtual('password')
+    .set(function (password) {
+        this._password = password
+        this.salt = this.makeSalt()
+        this.hashed_password = this.encryptPassword(password) //Método del User 
+    })
+    .get(function () {
+        return this._password
+    })
+
+UserSchema.methods = {
+    authenticate: function (plaintext) {
+        return this.encryptPassword(plaintext) === this.hashed_password
+    },
+    encryptPassword: function (password) {
+        if (!password) return ''
+        try {
+            return crypto
+                .createHmac('sha1',this.salt)
+                .update(password)
+                .digest('hex')
+        } catch (error) {
+            return('Ocurrio un error en el metodo encryptpassword del metodo encryptpassword del modelo de usuario')
+        }
+    },
+    makeSalt: function () {
+        return Math.round((new Date().valueOf() * Math.random())) + ''
+    }
+}
+
 export default mongoose.model('User', UserSchema)
