b5a09db5a91dd2e5f31988a551d1d98fa71b5f0d
LuisGarcia <luis_garcia_11@outlook.es>
2021-02-19 08:01:05 -0600

    Finalizando controladores en archivo user.controller.js

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,6 +1,6 @@
-import User from '../models/user.model'
-import extend from 'lodash/extend'
-import errorHandler from './error.controller'
+import User from '../models/user.model';
+import extend from 'lodash/extend';
+import errorHandler from './error.controller';
 
 const create = async (req, res, next) => {
     const user = new User(req.body);
@@ -16,14 +16,68 @@ const create = async (req, res, next) => {
     }
 }
 
-const list = (req, res) => {}
+const list = (req, res) => {
+    try {
+        let users = await User.find().select('name email created');
+        res.json(users);
+    } catch (err) {
+        return res.status(400).json({
+            error: errorHandler.getErrorMessage(err)
+        });
+    }
+}
 
-const userById = (req, res, next, id) => {}
+const userById = (req, res, next, id) => {
+    try {
+        let user = await User.findById(id);
+        if (!user) {
+            return res.status('400').json({ /*Si algo sale mal revisar ese status('400') ¿deberia ir el 400 entre comillas? */
+                error: "user not found"
+            })
+        }
+        req.profile = user;
+        next()
+    } catch (err) {
+        return res.status('400').json({ /*Si algo sale mal revisar ese status('400') ¿deberia ir el 400 entre comillas? */
+            error: "Could not retrieve user"
+        })
+    }
+}
 
-const read = (req, res) => {}
+const read = (req, res) => { 
+    req.profile.hashed_password = undefined;
+    req.profile.salt = undefined;
+    return res.json(req.profile);
+}
 
-const update = (req, res, next) => {}
+const update = (req, res, next) => { 
+    try {
+        let user = req.profile;
+        user = extend(user, req.body); //¿Que hace 'extend()'?
+        user.updated = Date.now();
+        await user.save();
+        user.hashed_password = undefined;
+        user.salt = undefined;
+        res.json(user)
+    } catch (err) {
+        return res.status(400).json({
+            error: errorHandler.getErrorMessage(err)
+        });
+    }
+}
 
-const remove = (req, res, next) => {}
+const remove = (req, res, next) => { 
+    try {
+        let user = req.profile;
+        let deletedUser = await user.remove();
+        deletedUser.hashed_password = undefined;
+        deletedUser.salt = undefined;
+        res.json(deletedUser);
+    } catch (err) {
+        return res.status(400).json({
+            error: errorHandler.getErrorMessage(err)
+        });
+    }
+}
 
-export default {create, list, userById, read, remove, update}
+export default { create, list, userById, read, remove, update }
