dc8787c8024d22fd0443724a30ea4e320dd4d98e
LuisGarcia <luis_garcia_11@outlook.es>
2021-03-01 20:45:06 -0600

    Se instalan dependencias y se incluyen archivos de configuracion webpack para el lado del cliente

---
+++ webpack.config.client.production.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const webpack = require('webpack');
+const CURRENT_WORKING_DIR = process.cwd();
+const nodeExternals = require('webpack-node-externals')
+
+const config = {
+    mode: "production",
+    entry: [
+        path.join(CURRENT_WORKING_DIR, 'client/main.js')
+    ],
+    output: {
+        path: path.join(CURRENT_WORKING_DIR, '/dist'),
+        filename: 'bundle.js',
+        publicPath: "/dist/"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: [
+                    'babel-loader'
+                ]
+            }
+        ]
+    }
+}
+
+module.exports = config;
